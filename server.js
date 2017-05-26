ce1b91e88aaf8ed7707fa772c33f7007022cda92
Luca Nils Schmid <me@lucaschmid.net>
2017-05-26 22:32:38 +0200

    fix(server.js): add health endpoints to root

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const compress = require('koa-compress')
 const config = require('./config')
 const routes = require('./routes')
 const middleware = require('./middleware')
+const sendfile = require('koa-sendfile')
+const r = require('koa-router')()
+const path = require('path')
 
 app.name = 'npm-register'
 app.port = config.port
@@ -19,6 +22,18 @@ app.use(compress())
 app.use(middleware.error)
 app.use(routes.routes())
 app.use(routes.allowedMethods())
+
+r.get('/', function * () {
+  yield sendfile(this, path.join(__dirname, 'public/index.html'))
+})
+
+r.get('/-/ping', function * () {
+  this.body = {}
+})
+
+app.use(r.routes())
+app.use(r.allowedMethods())
+
 app.use(middleware.notfound)
 
 module.exports = app
